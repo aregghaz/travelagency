78d3af83918f9f67971c3e99aab4b7b2c8cf03bc
Areg Ghazaryan <aregghaz@gmail.com>
2017-11-03 14:02:30 +0400

    bugs

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -72,17 +72,17 @@
             <div class="form-group">
                 <input type="file" required="required" multiple="multiple" placeholder="image 1" name="img1" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 2" name="img2" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 2" name="img2" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 3" name="img3" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 3" name="img3" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 4" name="img4" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 4" name="img4" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 5" name="img5" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 5" name="img5" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 6" name="img6" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 6" name="img6" class="form-control">
             </div><div class="form-group">
-                <input type="file" required="required" multiple="multiple" placeholder="image 7" name="img7" class="form-control">
+                <input type="file"  multiple="multiple" placeholder="image 7" name="img7" class="form-control">
             </div>
             <input type="submit" name='post_comment' class="btn btn-success" value = "Post"/>
         </form>
