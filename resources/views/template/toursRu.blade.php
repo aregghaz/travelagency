eea155a5684d027c12a600fdb5e6d5eee145866d
Areg <aregghaz@gmail.com>
2017-11-14 23:52:49 -0800

    some_changes

--- resources/views/template/toursRu.blade.php
+++ resources/views/template/toursRu.blade.php
@@ -46,11 +46,11 @@
                     <div class="image_thumb_block">
                         <div class="gallery_carousel clear">
                             <div class="slide" style="width: 1910px;">
-                                <a data-toggle="modal" data-target=".{{$posts->img1}}" >
+                                <a data-toggle="modal" data-target=".{{$posts->id}}" >
 
                                     <div class="img" style="background: url('{{  URL::to("/images/turs/$posts->img1") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                 </a>
-                                <div class="modal fade {{$posts->img1}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                     <div class="modal-dialog modal-lg" role="document">
 
                                         <img src="{{  URL::to("/images/turs/$posts->img1") }}" alt="">
@@ -58,13 +58,13 @@
                                     </div>
                                 </div>
                                 @if(!empty($posts->img2))
-                                    <a data-toggle="modal" data-target=".{{$posts->img2}}" >
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}" >
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img2") }}') no-repeat;background-size: cover;background-position: center;">
 
                                         </div>
                                     </a>
-                                    <div class="modal fade {{$posts->img2}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
                                             <img src="{{  URL::to("/images/turs/$posts->img2") }}" alt="">
@@ -73,11 +73,11 @@
                                     </div>
                                 @endif
                                 @if(!empty($posts->img3))
-                                    <a data-toggle="modal" data-target=".{{$posts->img3}}" >
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}" >
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img3") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                     </a>
-                                    <div class="modal fade {{$posts->img3}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
                                             <img src="{{  URL::to("/images/turs/$posts->img3") }}" alt="">
@@ -86,11 +86,11 @@
                                     </div>
                                 @endif
                                 @if(!empty($posts->img4))
-                                    <a data-toggle="modal" data-target=".{{$posts->img4}}">
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}">
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img4") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                     </a>
-                                    <div class="modal fade {{$posts->img4}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
                                             <img src="{{  URL::to("/images/turs/$posts->img4") }}" alt="">
@@ -100,24 +100,24 @@
                                 @endif
 
                                 @if(!empty($posts->img5))
-                                    <a data-toggle="modal" data-target=".{{$posts->img5}}">
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}">
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img5") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                     </a>
-                                    <div class="modal fade {{$posts->img5}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
-                                            <img src="{{  URL::to("/images/turs/$posts->img5") }}" alt="">
+                                            <img src="{{  URL::to("/images/turs/$posts->id") }}" alt="">
 
                                         </div>
                                     </div>
                                 @endif
                                 @if(!empty($posts->img6))
-                                    <a data-toggle="modal" data-target=".{{$posts->img6}}">
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}">
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img6") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                     </a>
-                                    <div class="modal fade {{$posts->img6}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
                                             <img src="{{  URL::to("/images/turs/$posts->img6") }}" alt="">
@@ -126,11 +126,12 @@
                                     </div>
                                 @endif
                                 @if(!empty($posts->img7))
-                                    <a data-toggle="modal" data-target=".{{$posts->img7}}">
+                                    <a data-toggle="modal" data-target=".{{$posts->id}}">
                                         <div class="img"
                                              style="background: url('{{  URL::to("/images/turs/$posts->img7") }}') no-repeat;background-size: cover;background-position: center;"></div>
                                     </a>
-                                    <div class="modal fade {{$posts->img7}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+
+                                    <div class="modal fade {{$posts->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                                         <div class="modal-dialog modal-lg" role="document">
 
                                             <img src="{{  URL::to("/images/turs/$posts->img7") }}" alt="">
