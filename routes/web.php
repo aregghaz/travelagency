da19e03e4aed75b843e222d93829bf9c03790e78
Areg <aregghaz@gmail.com>
2017-11-15 00:15:09 -0800

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -13,45 +13,42 @@
 
 Route::get('/', ['as' => 'homeEn', 'uses' => 'UserController@homeEn']);
 Route::get('/homeRu', ['as' => 'homeRu', 'uses' => 'UserController@homeRu']);
-Route::get('/home',['as' => 'home', 'uses' => 'PostController@index']);
-Route::get('/login',['as' => 'login', 'uses' => 'UserController@login']);
-Route::post('/auth',['as' => 'auth', 'uses' => 'UserController@auth']);
-Route::get('/toursEn',['as' => 'toursEn', 'uses' => 'UserController@toursEn']);
-Route::get('/toursRu',['as' => 'toursRu', 'uses' => 'UserController@toursRu']);
-Route::get('/allToursRu',['as' => 'allToursRu', 'uses' => 'UserController@allToursRu']);
+Route::get('/home', ['as' => 'home', 'uses' => 'PostController@index']);
+Route::get('/login', ['as' => 'login', 'uses' => 'UserController@login']);
+Route::post('/auth', ['as' => 'auth', 'uses' => 'UserController@auth']);
+Route::get('/toursEn', ['as' => 'toursEn', 'uses' => 'UserController@toursEn']);
+Route::get('/toursRu', ['as' => 'toursRu', 'uses' => 'UserController@toursRu']);
+Route::get('/allToursRu', ['as' => 'allToursRu', 'uses' => 'UserController@allToursRu']);
 Route::get('/allToursEn', ['as' => 'allToursEn', 'uses' => 'UserController@allToursEn']);
 
 
+// show new post form
+//  Route::get('new-post','PostController@create');
 
-    // show new post form
-  //  Route::get('new-post','PostController@create');
+// save new post
+Route::post('new-post', ['uses' => 'PostController@store', 'as' => 'new-post']);
 
-    // save new post
-    Route::post('new-post',['uses' =>'PostController@store','as' =>'new-post' ]);
+// edit post form
+Route::get('edit/{slug}', 'PostController@edit');
 
-    // edit post form
-    Route::get('edit/{slug}','PostController@edit');
+// update post
+Route::post('update', 'PostController@update');
 
-    // update post
-    Route::post('update','PostController@update');
+// delete post
+Route::get('delete/{id}', 'PostController@destroy');
 
-    // delete post
-    Route::get('delete/{id}','PostController@destroy');
+// display user's all posts
+Route::get('my-all-posts', 'UserController@user_posts_all');
 
-    // display user's all posts
-    Route::get('my-all-posts','UserController@user_posts_all');
-
-    // display user's drafts
-    Route::get('my-drafts','UserController@user_posts_draft');
-
-
+// display user's drafts
+Route::get('my-drafts', 'UserController@user_posts_draft');
 
 
 //users profile
-Route::get('user/{id}','UserController@profile')->where('id', '[0-9]+');
+Route::get('user/{id}', 'UserController@profile')->where('id', '[0-9]+');
 
 // display list of posts
-Route::get('user/{id}/posts','UserController@user_posts')->where('id', '[0-9]+');
+Route::get('user/{id}/posts', 'UserController@user_posts')->where('id', '[0-9]+');
 
 //// display single post
 //Route::get('/{slug}',['as' => 'post', 'uses' => 'PostController@show'])->where('slug', '[A-Za-z0-9-_]+');
