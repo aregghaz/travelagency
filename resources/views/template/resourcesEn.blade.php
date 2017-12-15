9520e1285d8d6f4304a800e0bc434b625c506fa0
Areg Ghazaryan <aregghaz@gmail.com>
2017-12-15 14:44:40 +0400

    asdas

--- resources/views/template/resourcesEn.blade.php
+++ resources/views/template/resourcesEn.blade.php
@@ -9,29 +9,39 @@
         </div>
         <div class="col-md-8">
             <br><br>
-            <h3>Armenia</h3>
-            <p>
+            <h3 class="text-header"><strong>Resources</strong></h3>
+            <br><br>
+            <p class="read-more-wrap">
+                Armenia is an ancient country with great and rich historical, religious and cultural resources. This is a great place for travelling. It can be introduced to the tourists with its geographical diversity, wonderful nature and fascinating cultural heritage.                <br>
                 <br><br>
-                Present day Armenia<br><br>
-                Armenia, officially the Republic of Armenia, is a mountainous country in the South Caucasus region of Eurasia between the Black Sea and the Caspian Sea. Located in West Asia on the Armenian Highlands, it is bordered by Turkey to the west, Georgia to the north, the de facto independent Republic of Artsakh and Azerbaijan to the east, and Iran and Azerbaijan's exclave of Nakhchivan to the south.<br><br>
-                Armenia is a unitary, multiparty, democratic nation-state and one of the oldest and most historic civilizations in the world with a rich cultural heritage, as well as the first nation to adopt Christianity as its official religion. Although Armenia is constitutionally a secular state, the Christian faith plays a major role in both its history and the identification of the Armenian people.<br><br>
-                Culturally, historically, and politically, Armenia is considered to be part of Europe. However, its location in the southern Caucasus means that it can also be considered to be at the arbitrary border between Europe and Asia: in other words, a transcontinental nation.<br><br>
-                Armenia is currently a member of more than 35 different international organizations including the United Nations, the Council of Europe, Asian Development Bank, the Commonwealth of Independent States, World Trade Organization and Organization of the Black Sea Economic Cooperation. It is a Partnership for Peace (PfP) member of the North Atlantic Treaty Organisation and in a military alliance of CSTO. It is also an observer member of the Eurasian Economic Community, La Francophonie, and the Non-Aligned Movement. Armenia is also active in the international sports community with full membership in the Union of European Football Associations and International Ice Hockey Federation.<br><br>
-                <br>
+                Armenia was the first country to adopt Christianity, which is at the basis of the Armenian culture and statehood: Armenia is particularly rich in <strong>Christian historico-cultural monuments</strong>. Among them are  the Cathedral of  
+                <a href="https://en.wikipedia.org/wiki/Etchmiadzin_Cathedral" target="_blank">Etchmiadzin</a>, the Monasteries of
+                <a href="https://en.wikipedia.org/wiki/Noravank" target="_blank">Noravank</a>,
+                <a href="https://en.wikipedia.org/wiki/Geghard" target="_blank">Geghard</a>, 
+                <a href="https://en.wikipedia.org/wiki/Khor_Virap" target="_blank">Khor Virap</a>,
+                <a href="https://en.wikipedia.org/wiki/Goshavank" target="_blank">Goshavank</a>,
+                <a href="https://en.wikipedia.org/wiki/Lake_Sevan" target="_blank">Lake Sevan</a> and
+                <a href="https://en.wikipedia.org/wiki/Sevanavank" target="_blank">Sevanavank</a>, the ruins of the ancient temple of
+                <a href="https://en.wikipedia.org/wiki/Zvartnots_Cathedral" target="_blank">Zvartnots</a>, Monasteries of
+                <a href="https://en.wikipedia.org/wiki/Haghartsin_Monastery" target="_blank">Haghartsin</a>,
+                <a href="https://en.wikipedia.org/wiki/Saghmosavank" target="_blank">Saghmosavank</a>,
+                <a href="https://en.wikipedia.org/wiki/Haghpat_Monastery" target="_blank">Haghpat</a>,
+                <a href="https://en.wikipedia.org/wiki/Sanahin_Monastery" target="_blank">Sanahin</a>,
+                <a href="https://en.wikipedia.org/wiki/Tatev_Monastery" target="_blank">Tatev</a>,
+                <a href="https://en.wikipedia.org/wiki/Amberd" target="_blank">Amberd</a> fortress  and many others.<br><br>
+                There are also monuments of the pre-Christian era: the ruins of the Urartian Erebuni Teishebaini, the ancient Armenian capital of
+                <a href="https://en.wikipedia.org/wiki/Armavir_(ancient_city)" target="_blank">Armavir</a>,
+                <a href="https://en.wikipedia.org/wiki/Artaxata" target="_blank">Artaxata (Artashat)</a>, the pre-Christian temple of
+                <a href="https://en.wikipedia.org/wiki/Temple_of_Garni" target="_blank">Garni</a> and many others.  Armenia's rich history has left its unique imprint on each and every corner of a country, which for 3000 years had 12 capitals, many of which are represented in the modern Republic of Armenia.<br><br>
+                So many tourists visit Armenia to see the wonderful churches, cross stones and other castles and complexes that were built in early Christian times. Many museums, galleries and other cultural institutions and complexes can also be found in Armenia and especially in Yerevan.
                 <br><br>
-                Historical Armenia<br><br>
-                The Armenian civilization had its beginnings nearly 3000 years ago. Armenia lies in the highlands surrounding the Biblical mountains of Ararat. The original Armenian name for the country was Hayk, later Hayastan, translated as the land of Hayk. The legendary forefather of Armenians, Hayk, famous for his battles with Babylonian ruler Bel, most likely was one of the tribal leaders.<br><br>
-                The name Armenia was given to the country by the surrounding states, and it is traditionally derived from Armenak or Aram (the great-grandson of Hayk's great-grandson, and another leader who is, according to Armenian tradition, the ancestor of all Armenians).
-                Generally, historical Armenia is famous for its eminent kingdoms, which are:<br><br>
-                Kingdom of Van<br>
-                Orontid Dynasty<br>
-                Artaxiad dynasty<br>
-                Arsacid dynasty<br>
-                Bagratid dynasty<br>
-                Kingdom of Cilicia.<br><br>
-                Tigranes II, more commonly known as Tigranes the Great (140 – 55 BC) was King of Armenia under whom the country became, for a short time, the strongest state to Rome's east. He was a member of the Artaxiad Royal House. Under his reign, the Armenian kingdom expanded beyond its traditional boundaries, allowing Tigranes to claim the title "Great King", and involving Armenia in many battles against opponents such as the Parthian and Seleucid empires, and the Roman Republic.<br><br>
-                At its height, his empire extended from the Pontic Alps (in modern north-eastern Turkey) to Mesopotamia, and from the Caspian Sea to the Mediterranean. A series of victories led him to assume the Achaemenid title of "King of Kings", which even the Parthian kings did not assume, appearing on coins struck after 85 BC. Hence, the phrase "sea to sea Armenia" is a popular expression used by Armenians to refer to the kingdom of Tigranes, which extended from the Caspian Sea to the Mediterranean Sea.<br><br>
-                Over the course of his conquests, Tigranes founded four cities that bore his name, including the capital of Tigranocerta.
+                Furthermore, Armenia has several <strong>health resorts</strong> including Tsaghkadzor, Jermuk, Arzni, Dilijan, etc., where you can admire the beauty of nature, as well as have a great pleasure in sanatoriums.
+                <br><br>
+                If you are in love with <strong>adventurous</strong> tricks, then you should try to climb Mt. Aragats, go down Garni Gorge, etc.
+                <br><br>
+                So make sure Armenia is the country where you can fulfill all your desires and have a wonderful trip.
+                <br><br>
+
             </p><br><br>
         </div>
         <div class="col-md-2">
